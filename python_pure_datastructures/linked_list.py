4e730b8683f8cd2a10afbb531cd0e8e8a8a88bbd
shanmuk <shanmuksastry@gmail.com>
2020-05-19 10:20:21 +0530

    added delete for single linked list"

--- python_pure_datastructures/linked_list.py
+++ python_pure_datastructures/linked_list.py
@@ -46,7 +46,6 @@ class SingleLinkedList:
         while (i != position-1):
             node_to_iterate = node_to_iterate.get_next()
             i += 1
-
         node.set_next(node_to_iterate.get_next())
         node_to_iterate.set_next(node)
         self.length += 1
@@ -58,12 +57,46 @@ class SingleLinkedList:
             print(node_to_print.get_data())
             node_to_print = node_to_print.get_next()
 
+    def delete_from_beginning(self):
+        if not self.head:
+            return 'No elements are there'
+        self.head = self.head.get_next()
+        return True
 
-#
-# # Driver Program
-# linkedlist = SingleLinkedList()
-# linkedlist.insert(1)
-# linkedlist.insert(2)
-# linkedlist.insertAt(3,1)
-# linkedlist.insertAt(3,4)
+    def delete_from_end(self):
+        if not self.head:
+            return 'No elements are there'
+        node = self.head
+        while(node.get_next().get_next()):
+            node = node.get_next()
+        node.set_next(node.get_next().get_next())
+        return True
+
+    def delete_at(self, pos):
+        if pos >= self.length or pos < 0:
+            return 'Index out of range'
+        if pos == 0:
+            self.delete_from_beginning()
+        elif pos == self.length-1:
+            self.delete_from_end()
+        else:
+            i = 0
+            node = self.head
+            while (i != pos-1):
+                node = node.get_next()
+            node.set_next(node.get_next().get_next())
+        return 1
+
+
+# Driver Program
+linkedlist = SingleLinkedList()
+linkedlist.insert(1)
+linkedlist.insert(2)
+linkedlist.insertAt(3,1)
+linkedlist.insertAt(3,4)
+linkedlist.print_list()
+# linkedlist.delete_from_end()
+linkedlist.delete_at(3)
+linkedlist.print_list()
+# linkedlist.delete_from_beginning()
 # linkedlist.print_list()
