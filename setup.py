db2701bbfdcd51e4c9b44d4a76ba19cbc58decfc
shanmuk <shanmuksastry@gmail.com>
2020-05-19 09:06:03 +0530

    modded setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 
 from distutils.core import setup
 setup(
-  name = 'datastructures',         # How you named your package folder (MyLib)
-  packages = ['datastructures'],   # Chose the same as "name"
+  name = 'python_pure_datastructures',         # How you named your package folder (MyLib)
+  packages = ['pure_python_datastructures'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'All Datastructures and Algorithms',   # Give a short description about your library
   author = 'Shanmuk',                   # Type in your name
   author_email = 'reddragonofdreams@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/shanmuk184/datastructures.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/shanmuk184/datastructures/archive/0.4.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/shanmuk184/datastructures/archive/0.5.tar.gz',    # I explain this later on
   keywords = ['Node', 'LinkedList'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       ],
